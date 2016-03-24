f13e1fe5b1484bbddd876302b91bd551e4d6c7d6
juven1996 <juven1996@gmail.com>
2016-03-24 15:49:13 -0700

    Added restriction on users

--- application/config/constants.php
+++ application/config/constants.php
@@ -2,7 +2,8 @@
 
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
-
+define('ROLE_USER','user');
+define('ROLE_ADMIN','admin');
 // location of data folders
 define('DATAPATH', './data/');
 define('XMLSUFFIX', '.xml');
--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -11,50 +11,71 @@
  */
 class Application extends CI_Controller {
 
-	protected $data = array();   // parameters for view components
-	protected $id; // identifier for our content
+    protected $data = array();   // parameters for view components
+    protected $id; // identifier for our content
 
-	/**
-	 * Constructor.
-	 * Establish view parameters & load common helpers
-	 */
+    /**
+     * Constructor.
+     * Establish view parameters & load common helpers
+     */
 
-	function __construct()
-	{
-		parent::__construct();
-		$this->data = array();
-		$this->data['title'] = "Top Secret Government Site"; // our default title
-		$this->errors = array();
-		$this->data['pageTitle'] = 'welcome';   // our default page
-	}
+    function __construct() {
+        parent::__construct();
+        $this->data = array();
+        $this->data['title'] = "Top Secret Government Site"; // our default title
+        $this->errors = array();
+        $this->data['pageTitle'] = 'welcome';   // our default page
+    }
 
-	/**
-	 * Render this page
-	 */
-	function render()
-	{
-		$mychoices = array('menudata' => $this->makemenu());
-                $this->data['sessionid'] = session_id();
-		$this->data['menubar'] = $this->parser->parse('_menubar', $mychoices, true);
-		$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+    /**
+     * Render this page
+     */
+    function render() {
+        $mychoices = array('menudata' => $this->makemenu());
+        $this->data['sessionid'] = session_id();
+        $this->data['menubar'] = $this->parser->parse('_menubar', $mychoices, true);
+        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
 
-		// finally, build the browser page!
-		$this->data['data'] = &$this->data;
-		$this->parser->parse('_template', $this->data);
-	}
+        // finally, build the browser page!
+        $this->data['data'] = &$this->data;
+        $this->parser->parse('_template', $this->data);
+    }
 
-	// build menu choices depending on the user role
+    // build menu choices depending on the user role
 	function makemenu()
 	{
 		$choices = array();
-
-		$choices[] = array('name' => "Alpha", 'link' => '/alpha');
-		$choices[] = array('name' => "Beta", 'link' => '/beta');
-		$choices[] = array('name' => "Gamma", 'link' => '/gamma');
-		$choices[] = array('name' => "Login", 'link' => '/auth');
-		$choices[] = array('name' => "Logout", 'link' => '/auth/logout');
+		$userRole = $this->session->userdata('userRole');
+		if(strcmp($userRole, "admin") == 0){
+			$choices[] = array('name' => "Alpha", 'link' => '/alpha');
+			$choices[] = array('name' => "Beta", 'link' => '/beta');
+			$choices[] = array('name' => "Gamma",    'link' => '/gamma');
+			$choices[] = array('name' => "Logout", 'link' => '/auth/logout');
+		}else if(strcmp($userRole, "user") == 0){
+			$choices[] = array('name' => "Alpha", 'link' => '/alpha');
+			$choices[] = array('name' => "Beta", 'link' => '/beta');
+			$choices[] = array('name' => "Logout", 'link' => '/auth/logout');
+		}else {
+			$choices[] = array('name' => "Alpha", 'link' => '/alpha');
+			$choices[] = array('name' => "Login", 'link' => '/auth');
+		}
 		return $choices;
 	}
+
+    function restrict($roleNeeded = null) {
+        $userRole = $this->session->userdata('userRole');
+        if ($roleNeeded != null) {
+            if (is_array($roleNeeded)) {
+                if (!in_array($userRole, $roleNeeded)) {
+                    redirect("/");
+                    return;
+                }
+            } else if ($userRole != $roleNeeded) {
+                redirect("/");
+                return;
+            }
+        }
+    }
 
 }
 
